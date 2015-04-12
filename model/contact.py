a75b90b7cb184db9e533ce9917b37f3bc733f508
ek-makarenok <ek.makarenok@gmail.com>
2015-04-13 02:38:21 +0300

    Revert "Задание №11"
    This reverts commit c4b1cc4644075a48e8aea81da255ce4618b78d0e.

--- model/contact.py
+++ model/contact.py
@@ -1,12 +1,9 @@
 __author__ = 'makarenok'
 
 
-from sys import maxsize
-
-
 class Contact:
     def __init__(self, first_name=None, middle_name=None, last_name=None, nick=None, title=None,
-                 company=None, address=None, tel_home=None, email=None, id=None):
+                 company=None, address=None, tel_home=None, email=None):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
@@ -16,18 +13,3 @@ class Contact:
         self.address = address
         self.tel_home = tel_home
         self.email = email
-        self.id = id
-
-    def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.first_name, self.last_name)
-
-    def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and \
-               (self.first_name is None or other.first_name is None or self.first_name == other.first_name) and \
-               (self.last_name is None or other.last_name is None or self.last_name == other.last_name)
-
-    def id_or_max(self):
-        if self.id:
-            return int(self.id)
-        else:
-            return maxsize
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,19 +6,11 @@ from model.contact import Contact
 def test_modify_contact_first_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="testFirstName", middle_name="testMiddleName"))
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(first_name="New firstName")
-    contact.id = old_contacts[0].id
-    app.contact.modify_first_contact(contact)
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
-    old_contacts[0] = contact
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    app.contact.modify_first_contact(Contact(first_name="New firstName"))
 
-#def test_modify_contact_middle_name(app):
-#    if app.contact.count() == 0:
-#        app.contact.create(Contact(first_name="testFirstName", middle_name="testMiddleName"))
-#    old_contacts = app.contact.get_contact_list()
-#    app.contact.modify_first_contact(Contact(middle_name="New middleName"))
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts)== len(new_contacts)
+
+def test_modify_contact_middle_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="testFirstName", middle_name="testMiddleName"))
+    app.contact.modify_first_contact(Contact(middle_name="New middleName"))
+
