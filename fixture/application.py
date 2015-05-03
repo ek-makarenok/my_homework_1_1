90d9a50bed0f1335bad87d83b22c0d4b303feab9
ek-makarenok <ek.makarenok@gmail.com>
2015-05-03 04:43:08 +0300

    Задание №16
    Реализованы опции командной строки, позволяющие указывать при запуске
    тестов:
    - тип браузера,
    - адрес тестируемого приложения,
    - имя пользователя,
    - пароль.

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.contact import ContactHelper
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
@@ -7,12 +7,21 @@ __author__ = 'makarenok'
 
 
 class Application:
-    def __init__(self):
+    def __init__(self, browser, base_url="http://localhost/addressbook/"):
         # метод инициализирует фикстуру
-        self.wd = WebDriver()
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            # если браузер не определён, выбрасываем исключение
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +32,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         # метод разрушает фикстуру
