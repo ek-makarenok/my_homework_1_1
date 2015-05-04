2daa2ee0f4371d27476be6714b58aa444f944b62
ek-makarenok <ek.makarenok@gmail.com>
2015-05-05 02:06:55 +0300

    Задание №19
    Реализована загрузка тестовых данных из файлов в формате JSON.
    Тесты параметризованы.
    Для восстановления структуры объектов из формата JSON используется
    библиотека jsonpickle.

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+constant = [
+    Contact(firstname="firstname1", lastname="lastname1", homephone="homephone1", email="email11", email2="email2"),
+    Contact(firstname="firstname2", lastname="lastname2", homephone="homephone2", email="email21", email2="email22")
+]
+
+
--- generator/contact.py
+++ generator/contact.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from model.contact import Contact
 import getopt
 import jsonpickle
